637fee3a477ace7dfffa51790fd230c34e31aa4c
Forrest Guilloud <fbguillo@gmail.com>
2017-05-13 16:41:43 -0400

    added user routes import into app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const userRoutes = require('./lib/routes/user.routes');
 
 const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
 const port = process.env.PORT || 8100;
@@ -11,5 +12,7 @@ mongoose.connect(db);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
 
 app.listen(port, () => console.log('app listening on port: ', port));
