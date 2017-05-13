458682102dcfe6b81b44ff160c6a3595a0271e19
Forrest Guilloud <fbguillo@gmail.com>
2017-05-13 16:21:20 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const todoRoutes = require('./lib/routes/todo.routes');
+
+const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
+const port = process.env.PORT || 8100;
+
+mongoose.connect(db);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+app.use('/todos', todoRoutes);
+
+app.listen(port, () => console.log('app listening on port: ', port));
