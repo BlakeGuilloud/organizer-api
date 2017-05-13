48ac299dcf7ee8592aef001fb70266b431230b87
Forrest Guilloud <fbguillo@gmail.com>
2017-05-13 16:26:53 -0400

    initial express app up and running

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const todoRoutes = require('./lib/routes/todo.routes');
 
 const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
 const port = process.env.PORT || 8100;
@@ -12,7 +11,5 @@ mongoose.connect(db);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(bodyParser.json());
-
-app.use('/todos', todoRoutes);
 
 app.listen(port, () => console.log('app listening on port: ', port));
