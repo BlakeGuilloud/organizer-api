a8963e3c5270fb9e1f745ab9e0d2cbc0e4407679
Forrest Guilloud <fbguillo@gmail.com>
2017-05-13 16:46:27 -0400

    user creation complete

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,10 @@
-import { SendSucces, SendError } from 'tael';
+import { SendSuccess, SendError } from 'tael';
 
 import * as UserService from '../services/user.service';
 
 export function postUser(req, res) {
   return Promise.resolve(req)
-    .then((data) => UserService.postUser(data))
-    .then(SendSucces(res))
+    .then((data) => UserService.postUser(data.body))
+    .then(SendSuccess(res))
     .catch(SendError(res));
 }
