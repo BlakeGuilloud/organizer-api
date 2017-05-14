34c8b4e2121c8918b0bc88798249e6967721d4ed
Forrest Guilloud <fbguillo@gmail.com>
2017-05-14 12:54:41 -0400

    using dotenv and ignore env file.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const userRoutes = require('./lib/routes/user.routes');
-const devEnv = require('./env.config');
 
-const db = process.env.MONGODB_URI || devEnv.MONGODB_URI || 'mongodb://localhost/testing';
+const db = process.env.MONGODB_URI || 'mongodb://localhost/testing';
 const port = process.env.PORT || 8100;
 
 mongoose.Promise = global.Promise;
