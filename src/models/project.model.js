a024943f3aae76f47f56653a44a9325179985ac7
Forrest Guilloud <fbguillo@gmail.com>
2017-05-15 23:32:15 -0400

    fixed some bad data

--- src/models/project.model.js
+++ src/models/project.model.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const ProjectSchema = new Schema({
   name: String,
   description: String,
-  userId: { type: String, ref: 'User' },
   tags: [{ type: String, ref: 'Tag' }]
 });
 
